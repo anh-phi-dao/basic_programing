831de79dd67ac6840bb185a7ad91876a1c920e18
DaoAnhPhi-Gigabyte-PC <freeakaphi2k3@gmail.com>
2025-10-26 13:02:37 +0700

    change comments

--- include/someone.h
+++ include/someone.h
@@ -52,13 +52,54 @@ enum
     COMPLETE
 };
 
+/**
+ * @brief generate random type of that person
+ * @param: someone_t *someone
+ * @return: int=0 -> NORMAL / int=1->COMPLETE / int = -1->ERROR
+ * **/
 int generate_people(someone_t *someone);
+/**
+ * @brief generate "antrom" type of person
+ * @param: someone_t *someone
+ * @return: the state of excecuting the function, ERROR= fail
+ *                                                COMPLETE =success
+ * **/
 int generate_anxin(someone_t *someone);
+/**
+ * @brief generate "antrom" type of person
+ * @param: someone_t *someone
+ * @return: the state of excecuting the function, ERROR= fail
+ *                                                COMPLETE =success
+ * **/
 int generate_antrom(someone_t *someone);
+/**
+ * @brief generate "congnhan" type of person
+ * @param: someone_t *someone
+ * @return: the state of excecuting the function, ERROR= fail
+ *                                                COMPLETE =success
+ * **/
 int generate_congnhan(someone_t *someone);
 
+/**
+ * @brief: action of "anxin"
+ * @param: someone_t *someone
+ * @return : char *storing_string
+ * @note: must check result string, if the string is NULL , that means this function has failed
+ * **/
 char *cadge(someone_t *person);
+/**
+ * @brief: action of "antrom"
+ * @param: someone_t *someone
+ * @return : char *storing_string
+ * @note: must check result string, if the string is NULL , that means this function has failed
+ * **/
 char *stole(someone_t *person);
+/**
+ * @brief: action of "congnhan"
+ * @param: someone_t *someone
+ * @return : char *storing_string
+ * @note: must check result string, if the string is NULL , that means this function has failed
+ * **/
 char *work(someone_t *person);
 
 #endif
--- src/someone.c
+++ src/someone.c
@@ -1,10 +1,5 @@
 #include "someone.h"
 
-/**
- * Purpose: generate random type of that person
- * @param: someone_t *someone
- * @return: int=0 -> NORMAL / int=1->COMPLETE / int = -1->ERROR
- * **/
 int generate_people(someone_t *someone)
 {
     int type_in_number = rand() % 3;
@@ -29,12 +24,6 @@ int generate_people(someone_t *someone)
     return state;
 }
 
-/**
- * Purpose: generate "anxin" type of person
- * @param: someone_t *someone
- * @return: the state of excecuting the function, ERROR= fail
- *                                                COMPLETE =success
- * **/
 int generate_anxin(someone_t *someone)
 {
     someone->type = (char *)malloc(sizeof(char) * 5);
@@ -59,12 +48,6 @@ int generate_anxin(someone_t *someone)
     return COMPLETE;
 }
 
-/**
- * Purpose: generate "antrom" type of person
- * @param: someone_t *someone
- * @return: the state of excecuting the function, ERROR= fail
- *                                                COMPLETE =success
- * **/
 int generate_antrom(someone_t *someone)
 {
     someone->type = (char *)malloc(sizeof(char) * 6);
@@ -89,12 +72,6 @@ int generate_antrom(someone_t *someone)
     return COMPLETE;
 }
 
-/**
- * Purpose: generate "congnhan" type of person
- * @param: someone_t *someone
- * @return: the state of excecuting the function, ERROR= fail
- *                                                COMPLETE =success
- * **/
 int generate_congnhan(someone_t *someone)
 {
     someone->type = (char *)malloc(sizeof(char) * 8);
@@ -111,12 +88,6 @@ int generate_congnhan(someone_t *someone)
     return COMPLETE;
 }
 
-/**
- * @brief: action of "anxin"
- * @param: someone_t *someone
- * @return : char *storing_string
- * @note: must check result string, if the string is NULL , that means this function has failed
- * **/
 char *cadge(someone_t *person)
 {
     char *storing_string = (char *)malloc(50 * sizeof(char));
@@ -129,12 +100,6 @@ char *cadge(someone_t *person)
     return storing_string;
 }
 
-/**
- * @brief: action of "antrom"
- * @param: someone_t *someone
- * @return : char *storing_string
- * @note: must check result string, if the string is NULL , that means this function has failed
- * **/
 char *stole(someone_t *person)
 {
     char *storing_string = (char *)malloc(10 * sizeof(char));
@@ -147,12 +112,6 @@ char *stole(someone_t *person)
     return storing_string;
 }
 
-/**
- * @brief: action of "congnhan"
- * @param: someone_t *someone
- * @return : char *storing_string
- * @note: must check result string, if the string is NULL , that means this function has failed
- * **/
 char *work(someone_t *person)
 {
     char *storing_string = (char *)malloc(50 * sizeof(char));
